f0ed223ceb7f728975cf1aa3d1cd18327baa3b29
James Fernandez <jffernan@comcast.net>
2017-10-06 15:32:43 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* 
+Build an ES6 President class with the following properties set by the constructor: name, politicalParty, yearsInOffice, and homeState.
+*/
 class President {
   constructor(name, politicalParty, yearsInOffice, homeState) {
     this.name = name;
@@ -5,6 +8,15 @@ class President {
     this.yearsInOffice = yearsInOffice;
     this.homeState = homeState;
   }
+  
+/*
+Add following methods to the class:
+1. veto — returns NO!
+2. passBill — returns You can do that!
+3. doCharity — returns I like to help people.
+4. conductPressInterview — returns I am proud to be an American.
+5. sayHi — returns Hi, my name is <name>. I am from <homestate>. I represent the <politicalParty>s, and was in office <yearsInOffice>.
+*/
   
   veto() {
     return 'NO!';
@@ -26,3 +38,5 @@ class President {
     return "Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + "."
   }
 }
+
+var GeorgeWashington = new President("George Washington", "Demo", 10, "DC");
